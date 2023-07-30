2d9a874dc73d9edf20078446b2e522364db5a5a5
1s22s1 <1s22s1@example.com>
2023-07-31 05:46:39 +0900

    Finish 5.2.2

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,30 +1,48 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
+
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
+
     type Query {
         totalPhots: Int!
+        allPhotos: [Photo!]!
     }
 
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(name: String! description: String): Photo!
     }
 `
 
+var _id = 0
 var photos = []
 
 const resolvers = {
     Query: {
-        totalPhots: () => photos.length
+        totalPhots: () => photos.length,
+        allPhotos: () => photos
     },
 
-    // argsにはnameとdescriptionが含まれる。
     Mutation: {
         postPhoto(parent, args) {
-            photos.push(args)
-            return true
-        }
-    }
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
 
+            return newPhoto
+        }
+    },
+
+    Photo: {
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    }
 }
 
 const server = new ApolloServer({
